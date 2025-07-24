de0dd28e28670f1a46e68ef9bb602f8bd8d4bc22
DarrenClarke19 <142377488+DarrenClarke19@users.noreply.github.com>
2025-07-23 21:50:15 -0400

    Update merge_strings_alternately.py

--- algorithms/strings/merge_strings_alternately.py
+++ algorithms/strings/merge_strings_alternately.py
@@ -1,3 +1,37 @@
+# Source : https://leetcode.com/problems/merge-strings-alternately/description/
+# Author : Darren Clarke
+# Date   : 23/07/2025
+
+"""
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order,
+starting with word1. If a string is longer than the other, append the additional letters onto the end 
+of the merged string.
+
+Return the merged string.
+
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: word2 is longer, so "rs" is appended at the end.
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: word1 is longer, so "cd" is appended at the end.
+
+Constraints:
+- 1 <= word1.length, word2.length <= 100
+- word1 and word2 consist of lowercase English letters.
+"""
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         merged = []

75aa4801808bda5aea09a4572b799ff6c80e2605
Darren Clarke <darrenjustinclarke@yahoo.com>
2025-09-02 22:29:04 -0400

    implement your plan-practice

--- practice-U-M-P/reverse-words-in-a-string-III.py
+++ practice-U-M-P/reverse-words-in-a-string-III.py
@@ -31,3 +31,9 @@ Begin
 End
 
 '''
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words=s.split(" ")
+        reversed_words=[word[::-1] for word in words]
+        return " ".join(reversed_words)
